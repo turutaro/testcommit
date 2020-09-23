8527aec09c710865529602493b80988a7b84cff5
鶴岡隆慶 <tsuruokatakayoshi@turunoMacBook-Air.local>
2020-09-23 17:15:19 +0900

    numbers

---
+++ dotinstall/numbersGame/numbers.js
@@ -0,0 +1,92 @@
+'use stricr'
+
+
+{
+    class Panel {
+        constructor() {
+            this.el = document.createElement('li');
+            this.el.classList.add('pressed');
+            this.el.addEventListener('click', () => {
+                this.check();
+            });
+        }
+
+        getEl() {
+            return this.el;
+        }
+
+        activate(num) {
+            this.el.classList.remove('pressed');
+            this.el.textContent = num;
+        }
+
+        check() {
+            if (currentNum === parseInt(this.el.textContent, 10)) {
+                this.el.classList.add('pressed');
+                currentNum++;
+
+                if (currentNum === 4) {
+                    clearTimeout(timeoutId);
+                    btn.textContent = 'RESTART';
+                    currentNum = 0;
+                    isPlaying = false;
+                }
+            }
+        }
+    }
+
+    class Board {
+        constructor() {
+            this.panels = [];
+            for (let i = 0; i < 4; i++) {
+                this.panels.push(new Panel());
+            }
+            this.setup();
+        }
+
+        setup() {
+            const board = document.getElementById('board');
+            this.panels.forEach(panel => {
+                board.appendChild(panel.getEl());//カプセル化
+            });
+        }
+
+        activate() {
+            const nums = [0, 1, 2, 3];
+
+            this.panels.forEach(panel => {
+                const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
+                panel.activate(num);
+            });
+        }
+    }
+
+    function runTimer() {
+        const timer = document.getElementById('timer');
+        timer.textContent = ((Date.now() - startTime) / 1000).toFixed(2);
+
+        timeoutId = setTimeout(() => {
+            runTimer();
+        },10);
+    }
+
+    const board = new Board();
+
+    let currentNum = 0;
+    let startTime;
+    let timeoutId;
+    let isPlaying = false;
+
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click', () =>{
+        if (isPlaying) {
+            return;
+        }
+        isPlaying = true;
+
+        board.activate();
+
+        startTime = Date.now();
+        runTimer();
+    });
+}
